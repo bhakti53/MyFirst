de9d8c6caddc140c8f040be7bc1132f65303063b
bhakti53 <63838208+bhakti53@users.noreply.github.com>
2020-04-17 00:58:42 -0600

    Added Multiplication code

--- Addition.java
+++ Addition.java
@@ -3,10 +3,13 @@ public class Addition {
 
    public static void main(String[] args) {
         
-      int num1 = 5, num2 = 15, sum;
+      int num1 = 5, num2 = 15, sum ,sub,mult;
       sum = num1 + num2;
-      sub=num2-num1:
+      sub=num2-num1;
+      mult=num1*num2;
       System.out.println("Sum of these numbers: "+sum);
-      System.out.println("sub of these number:"+sub);
+      System.out.println("sub of these number:"+sub); 
+      System.out.println("mult of these numbers:+mult);
+                         
    }
 }
