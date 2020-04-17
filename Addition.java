ed50c2ed7fe23b276562ddae1a17b70427f64a3f
bhakti53 <63838208+bhakti53@users.noreply.github.com>
2020-04-17 00:47:17 -0600

    subtraction code added

--- Addition.java
+++ Addition.java
@@ -5,7 +5,8 @@ public class Addition {
         
       int num1 = 5, num2 = 15, sum;
       sum = num1 + num2;
-
+      sub=num2-num1:
       System.out.println("Sum of these numbers: "+sum);
+      System.out.println("sub of these number:"+sub);
    }
 }
